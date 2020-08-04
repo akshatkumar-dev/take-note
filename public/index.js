3c08948aadbb5e050f4fb79682375a5af3c4e0df
Akshat Kumar <akshatkumar.dev@gmail.com>
2020-08-04 22:11:30 +0530

    Basic function is working, duplex communication set up

--- public/index.js
+++ public/index.js
@@ -1,4 +1,11 @@
 const socket = io("http://localhost:4000/");
-socket.on("joined",data=>{
-    console.log(data);
+socket.emit("initialize",url);
+let textInput = document.querySelector("#textInput");
+textInput.addEventListener("keyup",()=>{
+    let text = textInput.value;
+    socket.emit("message",text);
+})
+socket.on("message-updated",data=>{
+    let textInput = document.querySelector("#textInput");
+    textInput.value = data;
 })
--- server.js
+++ server.js
@@ -7,11 +7,41 @@ const io = require("socket.io")(http);
 
 
 app.get("/",(req,res)=>{
-    res.render("index");
+    res.redirect("/"+getUniqueId());
 })
-
+app.get("/:url",(req,res)=>{
+    const url = req.params.url;    
+    res.render("index",{url:url});
+})
 io.on("connection",socket=>{
-    console.log("done");
-    socket.emit("joined","first");
+    let url;
+    socket.on("initialize",data=>{
+        url = data;
+        socket.join(data);
+    })
+    socket.on("message",data=>{
+        socket.to(url).emit("message-updated",data);
+    })
 })
+const getUniqueId = ()=>{
+    const variables = ["a","b","c","d","e",
+    "f","g","h","i","j",
+    "k","l","m","n","o",
+    "p","q","r","s","t",
+    "u","v","w","x","y",
+    "z","1","2","3","4",
+    "5","6","7","8","9",
+    "0","-","A","B","C",
+    "D","E","F","G","H",
+    "I","J","K","L","M",
+    "N","O","P","Q","R",
+    "S","T","U","V","W",
+    "X","Y","Z"];
+    let url = "";
+    for(let i = 0;i<5;i++){
+        let temp = Math.floor(Math.random()*63);
+        url+=variables[temp];
+    }
+    return url;
+}
 http.listen(process.env.PORT||4000,()=>{console.log("Listening on port 4000")})
