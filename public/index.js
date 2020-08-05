519b03508de326d926a6aea45fe7e1cf5a11c3e5
Akshat Kumar <akshatkumar.dev@gmail.com>
2020-08-05 18:49:25 +0530

    Add styles,notes and word and character count

--- public/index.js
+++ public/index.js
@@ -9,8 +9,19 @@ socket.on("message-initialize",data=>{
     }
 })
 let textInput = document.querySelector("#textInput");
+let words = document.querySelector("#words");
+let characters = document.querySelector("#characters");
 textInput.addEventListener("keyup",()=>{
     let text = textInput.value;
+    if(text.length === 0){
+        words.textContent="Words: 0"
+    }
+    else{
+    let wordCount = 1;
+    text.replace(/\s+/g,(a)=>{wordCount++;});
+    words.textContent="Words: "+wordCount;
+}
+    characters.textContent = "Characters: " + text.replace(/\s+/g,"").length;
     socket.emit("message",text);
 })
 socket.on("message-updated",data=>{
